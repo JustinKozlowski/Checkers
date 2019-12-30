f13e856dacb98c671cb8b94715203d1bbd7cc5c4
Kozlowski <itcs.jjkozlow@itorg.ad.buffalo.edu>
2019-12-30 10:38:15 -0500

    Created checkers game and started data structure

---
+++ Checkers.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+
+struct grid {
+    char board[8]
+}
+
+struct boardState {
+    grid state[4]
+}
+
+struct gameState{
+     time[28]
+    int64_t red
+    int64_t redKing
+    int64_t black
+    int64_t blackKing
+    int64_t colorTurn
+    int64_t repetitionCount
+    int64_t 
+
+}
